f366aefe677c969f06485738925fa986337791bf
joni <jonipelaa1@gmail.com>
2026-06-16 04:56:49 +0300

    3.1-3.6

---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+let persons = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456"
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523" 
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345"
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122"
+  }
+]
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>') //etusivu
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+//yksittäine id
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(p => p.id === id)
+  if (person)
+  response.json(person)
+  else
+  response.status(404).end()
+})
+
+//poisto
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+//lisää uus
+app.post('/api/persons', (request, response) => {
+  const person = request.body
+
+  if (!person.name && !person.number){
+    return response.status(400).json({ error: 'name and number not set' })
+  }
+  else if (!person.name){
+    return response.status(400).json({ error: 'name not set' })
+  }
+  else if(!person.number){
+    return response.status(400).json({ error: 'number not set'})
+  }
+  else if(persons.find(p => p.name === person.name)){
+    return response.status(409).json({ error: 'name must be unique'})
+  }
+
+  person.id = String(Math.floor(Math.random() * 20000)) //20k väli
+  persons = persons.concat(person)
+
+  response.status(201).send(persons)
+})
+
+app.get('/info', (request, response) => {
+    response.send(`Phonebook has info for ${persons.length} people<br><br>${Date()}`)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
